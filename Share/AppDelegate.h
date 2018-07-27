a34ad90a1b2c58b0a11fa14a26db6ca1d89cd171
summerforver <122044024@qq.com>
2018-07-26 18:37:45 -0700

    Initial Commit

---
+++ Share/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Share
+//
+//  Created by 王一卓 on 2018/7/26.
+//  Copyright © 2018年 王一卓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
